9c999663a8a2fedafa360dc1b4e4f6a101eafd77
OmitHasanAdor <ibneshams05@gmail.com>
2026-06-08 01:57:23 +0600

    modified proxy js

--- src/proxy.js
+++ src/proxy.js
@@ -1,20 +1,24 @@
 import { NextResponse } from 'next/server'
 import { auth } from './lib/auth'
 import { headers } from 'next/headers'
- 
+
 // This function can be marked `async` if using `await` inside
 export async function proxy(request) {
 
     const session = await auth.api.getSession({
-    headers: await headers() // you need to pass the headers object.
-})
+        headers: await headers() // you need to pass the headers object.
+    })
 
-if (!session) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    if (!session) {
+        const currentUrl = request.nextUrl.pathname;
+        const loginUrl = new URL('/login', request.url);
+        loginUrl.searchParams.set('callbackUrl', currentUrl);
+        return NextResponse.redirect(loginUrl);
+    }
+    return NextResponse.next();
+
 }
 
-}
- 
 export const config = {
-  matcher: ['/tutors/:path*','/my-booked-sessions','/add-tutors','/my-tutors'],
+    matcher: ['/tutors/:path*', '/my-booked-sessions', '/add-tutors', '/my-tutors'],
 }
